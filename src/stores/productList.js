6a352ec726ee4eb740f133dd3b56e1ae5762b2d3
Sergei <lordistor@gmail.com>
2025-12-23 18:44:39 +0300

    добавлены уникальные id для добавления в корзину + мелкие правки

--- src/stores/productList.js
+++ src/stores/productList.js
@@ -1,12 +1,16 @@
 import { ref, computed } from 'vue'
 import { defineStore } from 'pinia'
 import { PRODUCTS } from '@/shared/constants'
+import { v4 } from 'uuid'
 
 export const useProductListStore = defineStore('productList', () => {
   const productList = ref(PRODUCTS)
   const getProductList = computed(() => productList.value)
+  const getProductById = (id) => {
+    return productList.value.filter((item) => item.id === +id)[0]
+  }
 
-  return { productList, getProductList }
+  return { productList, getProductList, getProductById }
 })
 
 export const useBasketStore = defineStore('basket', () => {
@@ -15,7 +19,7 @@ export const useBasketStore = defineStore('basket', () => {
   const getCount = computed(() => basket.value.length)
   const getPrice = computed(() => basket.value.reduce((acc, item) => acc + item.price, 0))
   const addToBasket = (product) => {
-    basket.value.push(product);
+    basket.value.push({...product, id: v4()});
   }
   const removeFromBasket = (id) => {
     basket.value = basket.value.filter((item) => item.id !== id)
